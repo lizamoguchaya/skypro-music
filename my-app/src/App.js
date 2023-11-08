ed479d4663bb7f94b953fcf0d578c34e846d5b26
lizamoguchaya <moguchaya-liza2013@yandex.ru>
2023-11-08 23:10:37 +0300

    3.1

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Здесь скоро будет Skypro.Music</p>
+    </div>
+  );
+}
+
+export default App;
